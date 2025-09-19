ec021cd97ef0bcb829ddce1d7a4b8efdbbbb18c6
ocurrentduke1 <a21110128@ceti.mx>
2025-09-18 20:48:46 -0600

    creacion de metodos crear y editar

--- src/app/components/create-park/create-park.ts
+++ src/app/components/create-park/create-park.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule, ActivatedRoute } from '@angular/router';
 import { ParkService } from '../../services/park-service';
+import { Park } from '../../models/park';
 
 @Component({
   selector: 'app-create-park',
@@ -9,7 +10,8 @@ import { ParkService } from '../../services/park-service';
   templateUrl: './create-park.html',
 })
 export class CreatePark {
-  parque = {
+  parque: Partial<Park> = {
+    id: undefined,
     park_name: '',
     park_abbreviation: '',
     park_img_uri: '',
@@ -39,7 +41,35 @@ export class CreatePark {
   }
 
   guardarParque() {
+    if (this.modoEdicion) {
+      this.actualizarParque();
+    } else {
+      // Remove 'id' from the object before creating, since it's not required for new parks
+      const { id, ...parqueSinId } = this.parque;
+      this.parkService.create(parqueSinId as Park).subscribe(() => {
+        this.router.navigate(['/parques']);
+      });
+    }
+  }
 
-    
+  actualizarParque() {
+    if (this.idEditar !== null) {
+      // Ensure all required Park properties are present and not undefined
+      const parqueConId: Park = {
+        id: this.idEditar,
+        park_name: this.parque.park_name ?? '',
+        park_abbreviation: this.parque.park_abbreviation ?? '',
+        park_img_uri: this.parque.park_img_uri ?? '',
+        park_address: this.parque.park_address ?? '',
+        park_city: this.parque.park_city ?? '',
+        park_state: this.parque.park_state ?? '',
+        park_zip_code: this.parque.park_zip_code ?? 0,
+        park_latitude: this.parque.park_latitude ?? 0,
+        park_longitude: this.parque.park_longitude ?? 0,
+      };
+      this.parkService.update(this.idEditar, parqueConId).subscribe(() => {
+        this.router.navigate(['/parques']);
+      });
+    }
   }
 }
